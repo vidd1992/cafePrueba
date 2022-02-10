0697b2cf8a26586db5c301859ccd07b334018696
dmejiamcuenca <dmejia@cuenca.gob.ec>
2022-02-10 00:01:29 -0500

    david prueba

---
+++ Model/Credentials.php
@@ -0,0 +1,8 @@
+<?php
+//Login data for the database. Use this file in all Models
+$host = "coffee.cs18nbncvmqr.us-east-1.rds.amazonaws.com";
+$user = "coffee";
+$passwd = "12345678-Aa";
+$database = "coffeedb";
+
+?>
